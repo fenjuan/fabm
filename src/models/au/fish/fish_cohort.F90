0ea2d79d004e4a493bf64674dd66d2ee5a604e36
MrAzana <nicolassm87@gmail.com>
2018-11-06 08:59:07 +0100

    More or less finished fish reproduction
    All changed and added lines marked with an arrow
    
    Added comments to individual lines

--- src/models/au/fish/fish_cohort.F90
+++ src/models/au/fish/fish_cohort.F90
@@ -34,7 +34,7 @@
       type (type_global_dependency_id)         :: id_Day
 !
 !     Model parameters
-      integer         :: cht_nC
+      integer         :: cht_nC,cht_nc_init
       real(rk)        :: cht_ext,cht_Tmin,cht_Tmax,cht_q_J,cht_q_A
       real(rk)        :: cht_lambda_1,cht_lambda_2,cht_A_mx,cht_W_opt,cht_alpha,cht_x_mu
       real(rk)        :: cht_xi_1 ,cht_xi_2,cht_k_e,cht_rho_1,cht_rho_2,cht_q_s
@@ -128,12 +128,13 @@
    call self%get_parameter(self%cht_Q10_z     ,'Q10_food'               , '[-]'               , 'Q10 value of zooplankton population growth'               , default=1.799_rk )
    
    call self%get_parameter(self%cht_R_day     ,'repro_day'              , '[-]'               , 'reproduction day of year for fish '                       , default=152.0_rk )
+   call self%get_parameter(self%cht_nc_init   ,'initial_cohorts'        , '[-]'               , 'number of cohorts present at simulation start '           , default=1 )
 
 
 !  allocate state variable pointers, according to user-defined number of cohorts(cht_nC)
-   allocate(self%id_N(self%cht_nC))
-   allocate(self%id_r_mass(self%cht_nC))
-   allocate(self%id_i_mass(self%cht_nC))
+   allocate(self%id_N(self%cht_nC+self%cht_nc_init))
+   allocate(self%id_r_mass(self%cht_nC+self%cht_nc_init))
+   allocate(self%id_i_mass(self%cht_nC+self%cht_nc_init))
 !   allocate(self%wb(self%Nclasses))
 
 
@@ -147,23 +148,23 @@
 !stop
 
    ! setup fish cohorts 
-   do n=1,self%cht_nC
+   do n=1,self%cht_nC+self%cht_nc_init
       write (name,"(A1,I02.2)") "N", n-1
       write (longname, "(A9, I02.2)") "abundance", n-1
       call self%register_state_variable(self%id_N(n),name, '-', longname,initial_value= 0._rk, minimum=0.0_rk)
       write (name,"(A6,I02.2)") "r_mass", n-1
       write (longname, "(A15, I02.2)") "reversible mass", n-1
-      call self%register_state_variable(self%id_r_mass(n),name,'kg/individual',longname,initial_value= 0._rk,minimum=0.0_rk)
+      call self%register_state_variable(self%id_r_mass(n),name,'kg/individual',longname,initial_value= self%cht_q_J*self%cht_w_b/(1+self%cht_q_J),minimum=0.0_rk)
       write (name,"(A6,I02.2)") "i_mass", n-1
       write (longname, "(A17, I02.2)") "irreversible mass", n-1
-      call self%register_state_variable(self%id_i_mass(n),name,'kg/individual',longname,initial_value=0._rk,minimum=0.0_rk)
+      call self%register_state_variable(self%id_i_mass(n),name,'kg/individual',longname,initial_value=self%cht_w_b/(1+self%cht_q_J),minimum=0.0_rk)
    end do
    
 ! setup zooplankton
 call self%register_state_variable(self%id_Z_N,'prey','ind./L^-1','food concentration',initial_value=self%cht_K,minimum=0.0_rk)
 
 !  register environmental dependencies
-call self%register_dependency(self%id_uTm,    standard_variables%temperature)
+call self%register_dependency(self%id_uTm,    standard_variables%temperature)  ! eventually, this shoud be replaced
 call self%register_dependency(self%id_Day,    standard_variables%number_of_days_since_start_of_the_year)
 
 #if 0
@@ -198,73 +199,113 @@ call self%register_dependency(self%id_Day,    standard_variables%number_of_days_
    class (type_fish_cohort),intent(in) :: self
    _DECLARE_ARGUMENTS_DO_SURFACE_
 !
-! !LOCAL VARIABLES:
+!  LOCAL VARIABLES:
 !  state variables
-   real(rk)   :: N,r_mass,i_mass,Z_N
-   real(rk)   :: d_N, d_rmass, d_imass, d_ZN
-   real(rk)    :: g, f,tot_r
-   integer    :: i
-   
-   !  Carriers for environment dependencies
-   real(rk)     :: uTm,Day
+   real(rk)                     :: N,r_mass,i_mass,Z_N
+!  derivatives
+   real(rk)                     :: d_N, d_rmass, d_imass, d_ZN
+!  local variables
+   real(rk),save                :: F_tot=0.0_rk
+   real(rk), allocatable, save  :: g_mass(:) !                                                                                                                                                              <==================<<<
+!                                           
+!  carriers for environment dependencies and logical switches
+   real(rk)                     :: uTm,Day
+   integer                      :: i, ix_repro !                                                                                                                                                            <==================<<<
+   integer                      :: tcheck=0
+   integer,save                 :: year=0
 
 !EOP
 !-----------------------------------------------------------------------
 !BOC
 ! Enter spatial loops (if any)
    _HORIZONTAL_LOOP_BEGIN_
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+! 1. retrieve variable values, update trackers & variables:
+!===============================================================================================
+    _GET_HORIZONTAL_(self%id_Z_N,Z_N)                                                           ! get zooplankton (food) concentration
+    
+    !  retrieve environmental dependencies:
+    _GET_(self%id_uTm,uTm)                                                                      ! get temperature. eventually, this shoud be replaced
+    _GET_GLOBAL_(self%id_Day,Day)                                                               ! get current julian day
+    
+    ! check & update year - used for selecting new cohorts to start. To be removed if
+    !                       cohort implementation changes or if functionality to get 
+    !                       simulation year identification is added to FABM
+    if (day==1 .AND. tcheck == 0) then
+        year=year+1
+        tcheck=1
+    elseif (day==1 .AND. tcheck == 1) then
+        year=year
+    else
+        tcheck=0
+    endif
+    
+    ! allocate dimensions for saved cohort-specific variables
+    if (.not. allocated(g_mass)) allocate(g_mass(self%cht_nC+self%cht_nc_init)) !                                                                                                                           <==================<<<
+             
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+! 2. now do fish:
+!===============================================================================================
+    do i=1,self%cht_nC ! loop over the cohorts
+        _GET_HORIZONTAL_(self%id_N(i),N)                                                        ! get cohort abundance
+        _GET_HORIZONTAL_(self%id_r_mass(i),r_mass)                                              ! get cohort reversible mass (reserves+gonads)
+        _GET_HORIZONTAL_(self%id_i_mass(i),i_mass)                                              ! get cohort irreversible mass (structural tissues, organs)
 
-! get zooplankton (food) concentration:
-_GET_HORIZONTAL_(self%id_Z_N,Z_N)
-
-!  retrieve environmental dependencies
-   _GET_(self%id_uTm,uTm)
-   _GET_GLOBAL_(self%id_Day,Day)
-
-! now do fish:
-if (Day >= self%cht_R_day .AND. Day <= self%cht_R_day + 1.0_rk) then  ! determine if reproduction is due
-    !-----------------------------------------------------------------------
-    !  fish reproduction
-    !-----------------------------------------------------------------------
-    do i=1,self%cht_nC
-        _GET_HORIZONTAL_(self%id_N(i),N)
-        _GET_HORIZONTAL_(self%id_r_mass(i),r_mass)
-        _GET_HORIZONTAL_(self%id_i_mass(i),i_mass)
-        if ! cohort is mature .AND. not starving
-            ! calculate the reproduction here
-        else
-            ! no reproduction from this cohort; no change in cohort reversible mass
-        endif
-        
-        ! Update differentials
-        _SET_SURFACE_ODE_(self%id_N,g)            ! QUESTION: sets all cohort differentials or only one?
-        _SET_SURFACE_ODE_(self%id_r_mass,tot_r)
-        _SET_SURFACE_ODE_(self%id_i_mass,f)
-    end do
-else                                                                  ! reproduction is not due
-    !-----------------------------------------------------------------------
-    !  fish dynamics
-    !-----------------------------------------------------------------------
-    do i=1,self%cht_nC
-        _GET_HORIZONTAL_(self%id_N(i),N)
-        _GET_HORIZONTAL_(self%id_r_mass(i),r_mass)
-        _GET_HORIZONTAL_(self%id_i_mass(i),i_mass)
-        if ! cohort is not empty
+        if (N>self%cht_ext) then ! if cohort is not empty or extinct
+    !===========================================================================================
+    ! 2.1 fish reproduction
+    !-------------------------------------------------------------------------------------------
+            if (Day >= self%cht_R_day .AND. Day <= self%cht_R_day + 1.0_rk .AND. ix_repro==0) then ! if reproduction is due                                                                                 <==================<<<
+                if (i_mass >= self%cht_x_f .AND. r_mass > self%cht_q_J*i_mass) then ! cohort is mature and non-starving
+                    g_mass(i) = (r_mass-self%cht_q_J*i_mass)                                    ! calculate total loss of r_mass due to reproduction (gonad mass)
+                    F_tot = F_tot + g_mass(i)*N*self%cht_k_r/self%cht_w_b                       ! add offspring from cohort i to total produced offspring                                                   <==================<<<
+                else ! cohort is not ready to reproduce
+                    g_mass(i) = 0.0_rk                                                          ! no change in r_mass from reproduction for cohort i
+                endif   
+                ix_repro = int(i/self%cht_nc)                                                   ! only calculate reproduction once per reproductive event; stop when all cohorts have been cycled through   <==================<<<
+            else ! reproduction is not due
+                ix_repro=0                                                                      ! reset reproductive switch                                                                                 <==================<<<
+                g_mass(i) = 0.0_rk                                                              ! no change in r_mass from reproduction for cohort i
+                F_tot = 0.0_rk                                                                  ! no reproduction the rest of the year
+            endif
+    !===========================================================================================
+    ! 2.2 fish dynamics
+    !-------------------------------------------------------------------------------------------
             ! calculate size-specific rates and parameters
             ! calculate temperature correction rates
-            ! do predation on other fish
+            ! do predation on other fish (looping over other fish)
             ! calculate total encounter rates and final growth rates
             ! calculate final mortality rates
             ! allocate growth to reversible/irreversible mass
-        else
-            ! rate of change is zero
+            d_N = 0.0_rk
+            d_imass = 0.0_rk
+            d_rmass = 0.0_rk
+        else ! cohort is empty
+            ! rates of change of epmpty cohorts are zero
+            d_N = 0.0_rk
+            d_imass = 0.0_rk
+            d_rmass = 0.0_rk     
         endif
-        _SET_SURFACE_ODE_(self%id_N,g)
-        _SET_SURFACE_ODE_(self%id_r_mass,tot_r)
-        _SET_SURFACE_ODE_(self%id_i_mass,f)
+    !===========================================================================================
+    ! 2.3 update fish rates of change
+    !-------------------------------------------------------------------------------------------
+        _SET_SURFACE_ODE_(self%id_N(i),d_N)
+        _SET_SURFACE_ODE_(self%id_r_mass(i),d_rmass)
+        _SET_SURFACE_ODE_(self%id_i_mass(i),d_imass)
     end do
-endif
+    !============================================================================================
+    ! 2.4 special case: new cohort
+    !--------------------------------------------------------------------------------------------
+    if (Day >= self%cht_R_day .AND. Day <= self%cht_R_day + 1.0_rk) then                     ! reproduction is due                                                                                           <==================<<<
+        _SET_SURFACE_ODE_(self%id_N(year+self%cht_nc_init),F_tot)                            ! start new cohort with the produced offspring                                                                  <==================<<<
+    endif
    
+    
+    ! do zooplankton
+    ! d_ZN = 
+     _SET_SURFACE_ODE_(self%id_Z_N,d_ZN)
+     
+    
 !   do i=1,self%cht_nC
 !      _GET_HORIZONTAL_(self%id_N(i),N)
 !      _GET_HORIZONTAL_(self%id_r_mass(i),r_mass)
